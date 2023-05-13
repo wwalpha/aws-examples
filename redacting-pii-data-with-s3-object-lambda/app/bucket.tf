d31716fcb9af0ad86f772aa16c868b197f395a07
wwalpha <wwalpha@gmail.com>
2023-05-13 07:42:14 +0000

    update

--- redacting-pii-data-with-s3-object-lambda/app/bucket.tf
+++ redacting-pii-data-with-s3-object-lambda/app/bucket.tf
@@ -18,3 +18,39 @@ resource "aws_s3control_object_lambda_access_point" "this" {
     }
   }
 }
+
+# ----------------------------------------------------------------------------------------------
+# Amazon S3 Bucket Policy
+# ----------------------------------------------------------------------------------------------
+resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
+  bucket = var.s3_bucket_name
+  policy = data.aws_iam_policy_document.allow_access_from_cloudfront.json
+}
+
+# ----------------------------------------------------------------------------------------------
+# AWS IAM Policy
+# ----------------------------------------------------------------------------------------------
+data "aws_iam_policy_document" "allow_access_from_cloudfront" {
+  depends_on = [aws_cloudfront_distribution.this]
+
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_name}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.this.arn]
+    }
+  }
+}
--- redacting-pii-data-with-s3-object-lambda/app/cloudfront.tf
+++ redacting-pii-data-with-s3-object-lambda/app/cloudfront.tf
@@ -1,3 +1,6 @@
+# ----------------------------------------------------------------------------------------------
+# Cloudfront Origin Access Control
+# ----------------------------------------------------------------------------------------------
 resource "aws_cloudfront_origin_access_control" "this" {
   name                              = "${var.prefix}-oac"
   origin_access_control_origin_type = "s3"
@@ -5,6 +8,9 @@ resource "aws_cloudfront_origin_access_control" "this" {
   signing_protocol                  = "sigv4"
 }
 
+# ----------------------------------------------------------------------------------------------
+# Cloudfront Distribution
+# ----------------------------------------------------------------------------------------------
 resource "aws_cloudfront_distribution" "this" {
   origin {
     domain_name              = data.aws_s3_bucket.this.bucket_regional_domain_name
@@ -13,11 +19,11 @@ resource "aws_cloudfront_distribution" "this" {
   }
 
   enabled             = true
-  comment             = "Some comment"
+  comment             = "redacting-pii-data-with-s3-object-lambda"
   default_root_object = "index.html"
 
   default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    allowed_methods  = ["GET", "HEAD", ]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = aws_s3control_object_lambda_access_point.this.id
 
