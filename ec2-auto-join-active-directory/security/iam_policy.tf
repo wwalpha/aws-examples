199378cda6c58c8b9f2d5005d3b2dce308981add
wwalpha <wwalpha@gmail.com>
2023-12-29 09:04:05 +0000

    update

--- ec2-auto-join-active-directory/security/iam_policy.tf
+++ ec2-auto-join-active-directory/security/iam_policy.tf
@@ -1,11 +1,4 @@
 # ----------------------------------------------------------------------------------------------
-# IAM Policy - AmazonSSMManagedInstanceCore
-# ----------------------------------------------------------------------------------------------
-data "aws_iam_policy" "ssm_managed_instance_core" {
-  name = "AmazonSSMManagedInstanceCore"
-}
-
-# ----------------------------------------------------------------------------------------------
 # IAM Policy - AmazonSSMDirectoryServiceAccess
 # ----------------------------------------------------------------------------------------------
 data "aws_iam_policy" "ssm_directory_service_access" {
