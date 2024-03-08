041d5348e45270518a3cdea756db235c590379f8
wwalpha <wwalpha@gmail.com>
2024-03-08 12:54:37 +0000

    update

--- enterprise-network/step3-app/sg.tf
+++ enterprise-network/step3-app/sg.tf
@@ -44,19 +44,3 @@ module "ingress_sg" {
   ]
 }
 
-# module "alb_sg" {
-#   source = "terraform-aws-modules/security-group/aws"
-
-#   name   = "alb_sg"
-#   vpc_id = var.vpc_id
-#   ingress_with_cidr_blocks = [
-#     {
-#       rule        = "http-80-tcp"
-#       cidr_blocks = "10.10.0.0/16"
-#     }
-#   ]
-#   egress_with_source_security_group_id = [{
-#     rule                     = "http-80-tcp"
-#     source_security_group_id = module.app_sg.security_group_id
-#   }]
-# }
