79f59f1e8a5c536ce48316999e4e92792b5e83fa
wwalpha <wwalpha@gmail.com>
2024-05-04 06:09:01 +0000

    update

--- route53-resolver-in-multiaccount/commons-networking/vpc.tf
+++ route53-resolver-in-multiaccount/commons-networking/vpc.tf
@@ -11,3 +11,13 @@ module "vpc" {
   public_subnets     = var.subnets_cidr_block_private
   enable_nat_gateway = true
 }
+
+# ----------------------------------------------------------------------------------------------
+# AWS Route - Transit Gateway
+# ----------------------------------------------------------------------------------------------
+resource "aws_route" "private" {
+  count                  = length(module.vpc.private_route_table_ids)
+  route_table_id         = module.vpc.private_route_table_ids[count.index]
+  destination_cidr_block = local.vpc_cidr_block_cloud
+  transit_gateway_id     = var.transit_gateway_id
+}
---
+++ route53-resolver-in-multiaccount/resources-after-all/sg.tf
@@ -0,0 +1,51 @@
+# ----------------------------------------------------------------------------------------------
+# AWS Security Group - Inbound Endpoint Security Group
+# ----------------------------------------------------------------------------------------------
+module "inbound_endpoint_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+  name   = "${var.prefix}_inbound_endpoint_sg"
+  vpc_id = var.vpc_id_central_dns
+
+  ingress_with_cidr_blocks = [
+    {
+      rule        = "dns-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      rule        = "dns-udp"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  egress_with_cidr_blocks = [
+    {
+      rule        = "all-all"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+}
+
+# ----------------------------------------------------------------------------------------------
+# AWS Security Group - Outbound Endpoint Security Group
+# ----------------------------------------------------------------------------------------------
+module "outbound_endpoint_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+  name   = "${var.prefix}_outbound_endpoint_sg"
+  vpc_id = var.vpc_id_central_dns
+
+  ingress_with_cidr_blocks = [
+    {
+      rule        = "dns-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      rule        = "dns-udp"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  egress_with_cidr_blocks = [
+    {
+      rule        = "all-all"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+}
