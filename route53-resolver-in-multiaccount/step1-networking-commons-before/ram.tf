88f2f0b12d0f7e8d408b839b5981da6ffd8d0b5d
wwalpha <wwalpha@gmail.com>
2024-05-04 01:37:35 +0000

    update

---
+++ route53-resolver-in-multiaccount/step1-networking-commons-before/ram.tf
@@ -0,0 +1,47 @@
+# ----------------------------------------------------------------------------------------------
+# AWS Resource Access Manager Share
+# ----------------------------------------------------------------------------------------------
+resource "aws_ram_resource_share" "this" {
+  name                      = "${var.prefix}_transit_gateway"
+  allow_external_principals = true
+}
+
+# ----------------------------------------------------------------------------------------------
+# AWS Resource Access Manager Resource Association - Transit Gateway
+# ----------------------------------------------------------------------------------------------
+resource "aws_ram_resource_association" "this" {
+  resource_arn       = aws_ec2_transit_gateway.this.arn
+  resource_share_arn = aws_ram_resource_share.this.arn
+}
+
+# ----------------------------------------------------------------------------------------------
+# AWS Resource Access Manager Principal Association - Central DNS
+# ----------------------------------------------------------------------------------------------
+resource "aws_ram_principal_association" "central_dns" {
+  principal          = var.aws_account_id_central_dns
+  resource_share_arn = aws_ram_resource_share.this.arn
+}
+
+# ----------------------------------------------------------------------------------------------
+# AWS Resource Access Manager Principal Association - Onpremise
+# ----------------------------------------------------------------------------------------------
+resource "aws_ram_principal_association" "onpremise" {
+  principal          = var.aws_account_id_onpremise
+  resource_share_arn = aws_ram_resource_share.this.arn
+}
+
+# ----------------------------------------------------------------------------------------------
+# AWS Resource Access Manager Principal Association - Workload Public
+# ----------------------------------------------------------------------------------------------
+resource "aws_ram_principal_association" "workload_public" {
+  principal          = var.aws_account_id_workload_public
+  resource_share_arn = aws_ram_resource_share.this.arn
+}
+
+# ----------------------------------------------------------------------------------------------
+# AWS Resource Access Manager Principal Association - Workload Private
+# ----------------------------------------------------------------------------------------------
+resource "aws_ram_principal_association" "workload_private" {
+  principal          = var.aws_account_id_workload_private
+  resource_share_arn = aws_ram_resource_share.this.arn
+}
---
+++ route53-resolver-in-multiaccount/step1-networking-commons-before/variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {}
+
+variable "aws_account_id_central_dns" {}
+
+variable "aws_account_id_onpremise" {}
+
+variable "aws_account_id_workload_public" {}
+
+variable "aws_account_id_workload_private" {}
