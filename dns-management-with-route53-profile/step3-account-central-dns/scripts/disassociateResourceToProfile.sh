e17ade0c69ee810aa546787a495bbc1fdd35892f
wwalpha <wwalpha@gmail.com>
2024-05-21 09:56:01 +0000

    update

--- dns-management-with-route53-profile/step3-account-central-dns/scripts/disassociateResourceToProfile.sh
+++ dns-management-with-route53-profile/step3-account-central-dns/scripts/disassociateResourceToProfile.sh
@@ -3,8 +3,9 @@
 # describe profile id
 profileId=$(aws route53profiles list-profiles | jq -r .ProfileSummaries[0].Id)
 
+echo $profileId
 # disassociate profile
-aws route53profiles disassociate-resource-from-profile ---profile-id $profileId --resource-arn $RESOURCE_ARN_HOSTED_ZONE1
-aws route53profiles disassociate-resource-from-profile ---profile-id $profileId --resource-arn $RESOURCE_ARN_HOSTED_ZONE2
-aws route53profiles disassociate-resource-from-profile ---profile-id $profileId --resource-arn $RESOURCE_ARN_RESOLVER_RULE1
-aws route53profiles disassociate-resource-from-profile ---profile-id $profileId --resource-arn $RESOURCE_ARN_RESOLVER_RULE2
+aws route53profiles disassociate-resource-from-profile --profile-id $profileId --resource-arn $RESOURCE_ARN_HOSTED_ZONE1
+aws route53profiles disassociate-resource-from-profile --profile-id $profileId --resource-arn $RESOURCE_ARN_HOSTED_ZONE2
+aws route53profiles disassociate-resource-from-profile --profile-id $profileId --resource-arn $RESOURCE_ARN_RESOLVER_RULE1
+aws route53profiles disassociate-resource-from-profile --profile-id $profileId --resource-arn $RESOURCE_ARN_RESOLVER_RULE2
