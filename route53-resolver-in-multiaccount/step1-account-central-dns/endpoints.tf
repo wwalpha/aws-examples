4a7a88f1ce0121d464a35b6f4550644d018c0ee5
wwalpha <wwalpha@gmail.com>
2024-05-21 06:59:24 +0000

    update

--- route53-resolver-in-multiaccount/step1-account-central-dns/endpoints.tf
+++ route53-resolver-in-multiaccount/step1-account-central-dns/endpoints.tf
@@ -50,48 +50,48 @@ resource "aws_vpc_endpoint_policy" "ssm" {
   policy          = local.iam_policy
 }
 
-# # ----------------------------------------------------------------------------------------------
-# # AWS VPC Endpoint - EC2Messages
-# # ----------------------------------------------------------------------------------------------
-# resource "aws_vpc_endpoint" "ec2messages" {
-#   vpc_id              = module.vpc.vpc_id
-#   service_name        = "com.amazonaws.ap-northeast-1.ec2messages"
-#   vpc_endpoint_type   = "Interface"
-#   subnet_ids          = module.vpc.private_subnets
-#   private_dns_enabled = true
-#   security_group_ids  = [aws_security_group.allow_tls.id]
-#   tags = {
-#     Name = "${var.prefix}-ec2messages"
-#   }
-# }
+# ----------------------------------------------------------------------------------------------
+# AWS VPC Endpoint - EC2Messages
+# ----------------------------------------------------------------------------------------------
+resource "aws_vpc_endpoint" "ec2messages" {
+  vpc_id              = module.networking.vpc_id
+  service_name        = "com.amazonaws.ap-northeast-1.ec2messages"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = module.networking.vpc_private_subnet_ids
+  private_dns_enabled = true
+  security_group_ids  = [aws_security_group.allow_tls.id]
+  tags = {
+    Name = "${var.prefix}-ec2messages"
+  }
+}
 
-# # ----------------------------------------------------------------------------------------------
-# # AWS VPC Endpoint Policy - EC2Messages
-# # ----------------------------------------------------------------------------------------------
-# resource "aws_vpc_endpoint_policy" "ec2messages" {
-#   vpc_endpoint_id = aws_vpc_endpoint.ec2messages.id
-#   policy          = local.iam_policy
-# }
+# ----------------------------------------------------------------------------------------------
+# AWS VPC Endpoint Policy - EC2Messages
+# ----------------------------------------------------------------------------------------------
+resource "aws_vpc_endpoint_policy" "ec2messages" {
+  vpc_endpoint_id = aws_vpc_endpoint.ec2messages.id
+  policy          = local.iam_policy
+}
 
-# # ----------------------------------------------------------------------------------------------
-# # AWS VPC Endpoint - SSMMessages
-# # ----------------------------------------------------------------------------------------------
-# resource "aws_vpc_endpoint" "ssmmessages" {
-#   vpc_id              = module.vpc.vpc_id
-#   service_name        = "com.amazonaws.ap-northeast-1.ssmmessages"
-#   vpc_endpoint_type   = "Interface"
-#   subnet_ids          = module.vpc.private_subnets
-#   private_dns_enabled = true
-#   security_group_ids  = [aws_security_group.allow_tls.id]
-#   tags = {
-#     Name = "${var.prefix}-ssmmessages"
-#   }
-# }
+# ----------------------------------------------------------------------------------------------
+# AWS VPC Endpoint - SSMMessages
+# ----------------------------------------------------------------------------------------------
+resource "aws_vpc_endpoint" "ssmmessages" {
+  vpc_id              = module.networking.vpc_id
+  service_name        = "com.amazonaws.ap-northeast-1.ssmmessages"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = module.networking.vpc_private_subnet_ids
+  private_dns_enabled = true
+  security_group_ids  = [aws_security_group.allow_tls.id]
+  tags = {
+    Name = "${var.prefix}-ssmmessages"
+  }
+}
 
-# # ----------------------------------------------------------------------------------------------
-# # AWS VPC Endpoint Policy - SSMMessages
-# # ----------------------------------------------------------------------------------------------
-# resource "aws_vpc_endpoint_policy" "ssmmessages" {
-#   vpc_endpoint_id = aws_vpc_endpoint.ssmmessages.id
-#   policy          = local.iam_policy
-# }
+# ----------------------------------------------------------------------------------------------
+# AWS VPC Endpoint Policy - SSMMessages
+# ----------------------------------------------------------------------------------------------
+resource "aws_vpc_endpoint_policy" "ssmmessages" {
+  vpc_endpoint_id = aws_vpc_endpoint.ssmmessages.id
+  policy          = local.iam_policy
+}
