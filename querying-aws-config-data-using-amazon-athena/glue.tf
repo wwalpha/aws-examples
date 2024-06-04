c7b2756687f5ce3c88caefa4990e49dc0947f241
wwalpha <wwalpha@gmail.com>
2024-06-04 10:30:33 +0000

    save

--- querying-aws-config-data-using-amazon-athena/glue.tf
+++ querying-aws-config-data-using-amazon-athena/glue.tf
@@ -3,14 +3,6 @@
 # ----------------------------------------------------------------------------------------------
 resource "aws_glue_catalog_database" "this" {
   name = "${var.prefix}-db"
-
-  create_table_default_permission {
-    permissions = ["SELECT"]
-
-    principal {
-      data_lake_principal_identifier = "IAM_ALLOWED_PRINCIPALS"
-    }
-  }
 }
 
 # ----------------------------------------------------------------------------------------------
@@ -89,12 +81,16 @@ resource "aws_glue_catalog_table" "config_snapshots" {
       name = "configurationitems"
       type = "array<struct<configurationItemVersion:string,configurationItemCaptureTime:string,configurationStateId:bigint,awsAccountId:string,configurationItemStatus:string,resourceType:string,resourceId:string,resourceName:string,ARN:string,awsRegion:string,availabilityZone:string,configurationStateMd5Hash:string,configuration:string,supplementaryConfiguration:map<string,string>,tags:map<string,string>,resourceCreationTime:string>>"
     }
-
-    # skewed_info {
-    #   skewed_column_names               = []
-    #   skewed_column_value_location_maps = []
-    #   skewed_column_values              = []
-    # }
   }
 }
 
+resource "aws_lakeformation_permissions" "this" {
+  permissions = ["SELECT", "ALTER", "DROP"]
+  principal   = aws_iam_role.lambda_bucket_event.arn
+
+  table_with_columns {
+    database_name = aws_glue_catalog_table.config_snapshots.database_name
+    name          = aws_glue_catalog_table.config_snapshots.name
+    wildcard      = true
+  }
+}
